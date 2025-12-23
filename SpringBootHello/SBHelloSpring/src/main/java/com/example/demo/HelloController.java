28c1eea1f4eb19f0025c7a666c99eee4adb6407c
Aakash Gangwar <90668043+Astrix3348@users.noreply.github.com>
2025-12-23 18:08:54 +0530

    Add files via upload

---
+++ SpringBootHello/SBHelloSpring/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class HelloController {
+
+	@GetMapping("/register")
+	public String home() {
+		return "register";
+	}
+	
+	@PostMapping("/register")
+	public String mapper() {
+		return "view";
+	}
+	
+	
+	
+	
+
+}
